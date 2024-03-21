ea181cadbcbf87a221bd9ebf9363be6d3be0b744
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-21 23:17:09 +0700

    Initial Commit

---
+++ L8.1/L8_1App.swift
@@ -0,0 +1,17 @@
+//
+//  L8_1App.swift
+//  L8.1
+//
+//  Created by Anatoliy Odinetskiy on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct L8_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
